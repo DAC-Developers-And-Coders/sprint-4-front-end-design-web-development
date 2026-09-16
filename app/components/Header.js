0e5808eb2ac0c9bdcfd411d08120418b430c2392
Gabriel-Novaga <gnovaga@hotmail.com>
2026-09-16 10:00:49 -0300

    adding the Sprint 3 files

---
+++ app/components/Header.js
@@ -0,0 +1,34 @@
+const Link = ({ texto, href }) => <li><a href={href}>{texto}</a></li>
+
+const Header = () =>
+{
+    const linkStrings = {
+        link1: {href: "#hero", texto: "Início"},
+        link2: {href: "#solution", texto: "Solução"},
+        link3: {href: "#target-audience", texto: "Público-Alvo"},
+        link4: {href: "#gallery", texto: "Galeria"},
+        link5: {href: "#team", texto: "Nossa Equipe"},
+        link6: {href: "#contact", texto: "Contato"},
+        link7: {href: "/login", texto: "Login"}
+    };
+
+    const links = Object.entries(linkStrings).map(([key, link]) => (
+        <Link key={key} texto={link.texto} href={link.href} />
+    ));
+    
+    return(
+        <>
+            <header className="main-header">
+                <h2>DAC - JOVI</h2>
+
+                <nav className="nav-header">
+                    <ul className="nav-header-menu">
+                        {links}
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+}
+
+export default Header;
--- app/page.js
+++ app/page.js
@@ -1,8 +1,24 @@
-import Image from "next/image";
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Solution from './components/Solution'
+import TargetAudience from './components/TargetAudience'
+import Gallery from './components/Gallery'
+import Contact from './components/Contact'
+import Team from './components/Team'
 
 export default function Home() {
   return (
-    <div>
-    </div>
+    <>
+      <Header />
+      
+      <main>
+        <Hero />
+        <Solution />
+        <TargetAudience />
+        <Gallery />
+        <Team />
+        <Contact />
+      </main>
+    </>
   );
 }
