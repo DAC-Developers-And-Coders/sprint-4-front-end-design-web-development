1c2c38e24c665a7c3456ad7d6e62671505421886
Gabriel-Novaga <gnovaga@hotmail.com>
2026-09-18 12:46:31 -0300

    Tailwind Contact

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -10,36 +10,32 @@ const Options = ({ link, asset, imgAlt, txt }) =>
 
 const Contact = () =>
 {
-    const links = ['https://www.linkedin.com/in/gabrielnovagapereira/', 'mailto:developersandcodersfiap@gmail.com', 'https://www.linkedin.com/in/andrevictorgoncalvesnascimento/'];
-    const assets = ['./assets/images/contact/LinkedIn.jpg', './assets/images/contact/Email.jpg'];
-    const imgAlts = ['LinkedIn icon', 'Gmail icon'];
-    const txts = ['Gabriel Novaga', 'Nosso Email', 'André Nascimento'];
-    const options = [];
-
-    for(let i = 0; i <= 2; i++){
-        if(i == 0 || i == 2)
-            options.push(<Options key={i} link={links[i]} asset={assets[0]} imgAlts={imgAlts[0]} txt={txts[i]}/>)
-        else
-            options.push(<Options key={i} link={links[i]} asset={assets[i]} imgAlts={imgAlts[i]} txt={txts[i]}/>)
-    }
+    const contacts = [
+        {id: 1, name:"Gabriel Novaga", image:"./assets/images/contact/LinkedIn.jpg", alt:"Gmail icon", link: "https://www.linkedin.com/in/gabrielnovagapereira/"},
+        {id: 2, name:"Nosso Email", image:"./assets/images/contact/Email.jpg", alt:"LinkedIn icon", link: "mailto:developersandcodersfiap@gmail.com"},
+        {id: 3, name:"André Nascimento", image:"./assets/images/contact/LinkedIn.jpg", alt:"Gmail icon", link: "https://www.linkedin.com/in/andrevictorgoncalvesnascimento/"}
+    ]
 
     return(
         <>
-            <section id="contact">
-                <h1>Contato</h1>
-                <div>
-                    <h3>
-                        Gostaria de falar conosco? Pode nos encontrar nas seguintes plataformas!
-                    </h3>
-                </div>
-                <div>
-                    {options}
+            <section id="contact" className="grid grid-cols-1 min-h-screen gap-8">
+                <h1 className="text-4xl pt-20 text-center h-30">Contato</h1>
+                <h2 className="text-2xl text-center h-10">Gostaria de falar conosco? Pode nos encontrar nas seguintes plataformas!</h2>
+                <div className="grid grid-cols-3 gap-3 mx-8">
+                    {contacts.map((contact) => (
+                        <div key={contact.id} className="border-3 rounded-2xl transition-all duration-700 hover:scale-102 p-3 flex flex-col gap-8 items-center text-center">
+                            <a href={contact.link}>
+                                <img className="w-100" src={contact.image}></img>
+                            </a>
+                            <h2 className="text-4xl">{contact.name}</h2>
+                        </div>
+                    ))}
                 </div>
 
-                <footer>
-                    <img src="./assets/images/DAC_logo_mini.png" alt="Dac icon" />
-                    <a href="https://www.fiap.com.br/" target="_blank"><img src="./assets/images/fiap_logo.png" alt="fiap icon" /></a>
-                    <a href="https://www.jovimobile.com/" target="_blank"><img src="./assets/images/jovi_logo_white.png" alt="jovi icon" /></a>
+                <footer className=" bg-black w-full h-25 mt-auto grid grid-cols-3 gap-2 items-center justify-items-center">
+                    <img className="w-37.5 h-fit" src="./assets/images/DAC_logo_mini.png" alt="Dac icon"/>
+                    <a href="https://www.fiap.com.br/" target="_blank"><img className="w-37.5 h-fit" src="./assets/images/fiap_logo.png" alt="fiap icon" /></a>
+                    <a href="https://www.jovimobile.com/" target="_blank"><img className="w-37.5 h-fit" src="./assets/images/jovi_logo_white.png" alt="jovi icon" /></a>
                 </footer>
             </section>
         </>
