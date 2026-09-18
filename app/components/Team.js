b58dc1ec13dfea5bb0a45cef33d4ec8169082598
Gabriel-Novaga <gnovaga@hotmail.com>
2026-09-18 16:37:31 -0300

    mac responsive

--- app/components/Team.js
+++ app/components/Team.js
@@ -9,14 +9,14 @@ const Team = () =>
     ]
     return(
         <>
-            <section id="team" className="grid grid-cols-1 h-screen gap-8 mx-8 content-start max-md:h-[140dvh]">
+            <section id="team" className="grid grid-cols-1 h-screen gap-8 mx-8 content-start min-xl:h-[110dvh] max-md:h-[140dvh]">
                 <h1 className="text-4xl pt-20 text-center h-30">Conheça nossa Equipe!</h1>
                 <h2 className="text-2xl text-center h-10">Developers and Coders</h2>
                 
                 <div className="flex flex-wrap justify-center gap-5 p-4">
                     {team.map((member) => (
-                        <div key={member.id} className="w-full h-80 max-md:h-52 md:w-[calc(33.3%-1.5rem)] border-3 rounded-2xl shadow-[0_0_50px] shadow-gray-700 transition-all duration-700 hover:scale-104 p-3 flex flex-col gap-8 max-md:gap-2">
-                            <h2 className="text-3xl text-center max-md:text-xl">{member.name}</h2>
+                        <div key={member.id} className="w-full h-80 max-md:h-52 md:w-[calc(33.3%-1.5rem)] border-3 rounded-2xl shadow-[0_0_50px] shadow-gray-700 transition-all duration-700 hover:scale-104 p-3 flex flex-col gap-8 max-md:gap-2 min-xl:gap-1">
+                            <h2 className="text-3xl text-center max-md:text-xl min-xl:text-2xl">{member.name}</h2>
                             <h3 className="text-2xl text-center max-md:text-lg">{member.rm}</h3>
                             <p className="text-xl text-center max-md:text-md">{member.text}</p> 
                         </div>
