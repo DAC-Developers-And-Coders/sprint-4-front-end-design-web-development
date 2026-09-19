420ce478625c2ad678929cab56e71f5f2a6d445d
Gabriel-Novaga <gnovaga@hotmail.com>
2026-09-19 14:17:03 -0300

    add target contact

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -14,7 +14,7 @@ const Contact = () =>
                 <div className="grid grid-cols-1 gap-3 mx-10 md:justify-items-center xl:grid-cols-3 xl:gap-6 2xl:gap-8">
                     {contacts.map((contact) => (
                         <div key={contact.id} className="border-3 rounded-2xl transition-all duration-700 hover:scale-102 p-3 flex flex-col gap-1 items-center text-center md:w-70 lg:w-100">
-                            <a href={contact.link}>
+                            <a href={contact.link} target="_blank">
                                 <img className="size-40 lg:size-65 2xl:size-70" src={contact.image}></img>
                             </a>
                             <h2 className="text-xl lg:text-3xl 2xl:text-4xl">{contact.name}</h2>
