dc67de6f9165edbb12cef59e66a643f1364db09a
Gabriel-Novaga <gnovaga@hotmail.com>
2026-09-19 12:26:56 -0300

    fix bug

--- app/components/Header.js
+++ app/components/Header.js
@@ -29,8 +29,7 @@ const Header = () =>
     
     return(
         <>
-            <header className="bg-black text-snow sticky top-0 z-50 left-0 right-0 w-full flex flex-wrap items-center justify-between px-8 h-20
-            md:h-15">
+            <header className="bg-black text-snow sticky top-0 z-50 left-0 right-0 w-full flex flex-wrap items-center justify-between px-8 h-20 md:h-15">
                 <h2 className="text-sm md:text-xl 2xl:text-3xl">DAC - JOVI</h2>
 
                 <nav>
